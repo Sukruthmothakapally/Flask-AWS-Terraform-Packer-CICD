94758818a6e36509829fb80caf8e86d85f52a3c5
Sukruth_Mothakapally <107236457+Sukruthmothakapally@users.noreply.github.com>
2023-04-18 12:20:17 -0700

    Update iam.tf

--- aws-infra/iam.tf
+++ aws-infra/iam.tf
@@ -1,22 +1,5 @@
-resource "aws_iam_role" "s3_role" {
-  name = "s3_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
 resource "aws_iam_role_policy_attachment" "s3_policy_attachment" {
-  role       = aws_iam_role.s3_role.name
+  role       = aws_iam_role.EC2-CSYE6225.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
@@ -45,5 +28,4 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "EC2InstanceProfile-CSYE6225"
 
   role = aws_iam_role.EC2-CSYE6225.name
-  role = aws_iam_role.s3_role.name
 }
