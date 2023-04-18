e3b6defd317e03b2479bb820a5b336ee609808e3
Sukruth_Mothakapally <107236457+Sukruthmothakapally@users.noreply.github.com>
2023-04-18 12:02:17 -0700

    Update iam.tf

--- aws-infra/iam.tf
+++ aws-infra/iam.tf
@@ -1,22 +1,23 @@
-resource "aws_iam_policy" "WebAppS3" {
-  name = "WebAppS3"
-  policy = jsonencode({
+resource "aws_iam_role" "s3_role" {
+  name = "s3_role"
+
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = [
-          "s3:PutObject",
-          "s3:GetObject",
-          "s3:DeleteObject"
-        ],
+        Action = "sts:AssumeRole"
         Effect = "Allow"
-        Resource = [
-          "arn:aws:s3:::${aws_s3_bucket.private_bucket.id}",
-          "arn:aws:s3:::${aws_s3_bucket.private_bucket.id}/*"
-        ]
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
       }
     ]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_policy_attachment" {
+  role       = aws_iam_role.s3_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
 resource "aws_iam_role" "EC2-CSYE6225" {
@@ -35,11 +36,6 @@ resource "aws_iam_role" "EC2-CSYE6225" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "WebAppS3_policy_attachment" {
-  policy_arn = aws_iam_policy.WebAppS3.arn
-  role       = aws_iam_role.EC2-CSYE6225.name
-}
-
 resource "aws_iam_role_policy_attachment" "CloudWatch_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
   role       = aws_iam_role.EC2-CSYE6225.name
@@ -47,6 +43,7 @@ resource "aws_iam_role_policy_attachment" "CloudWatch_policy_attachment" {
 
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "EC2InstanceProfile-CSYE6225"
-  role = aws_iam_role.EC2-CSYE6225.name
-}
 
+  role = aws_iam_role.EC2-CSYE6225.name
+  role = aws_iam_role.s3_role.name
+}
